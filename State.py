4889402930223fb3e8ac29e7e546842e99ecb11e
Wildan Ghiffarie Budhi <wildangb2@gmail.com>
2019-03-26 18:41:33 +0700

    Bismillah

--- State.py
+++ State.py
@@ -4,9 +4,13 @@ class State():
 
     bitmask = BitMask()
 
-    def __init__(self, state):
+    def __init__(self, state, depth, alpha=0, beta=None, isPruning=False):
         self.state = self.bitmask.Hash(state)
         self.size = len(state)
+        self.depth = depth
+        self.alpha = alpha
+        self.beta = beta
+        self.isPruning = False
     
     def getTable(self):
         return self.bitmask.UnHash(self.state, self.size)
