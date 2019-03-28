0e5654b12950916ce9c1f2803f833398de842dcb
ranggakd <ranggakd@users.noreply.github.com>
2019-03-28 21:27:17 +0700

    ini commit rangga

--- Js/renderer.js
+++ Js/renderer.js
@@ -5,20 +5,25 @@ function python() {
     let options = {
         pythonPath: 'C:/ProgramData/Anaconda3/python.exe',
         scriptPath: 'C:/Users/comp/Documents/Python/KB/Alpha-Beta Pruning - Tic Tac Toe/Python/'
+        // args: ['value1']
     };
     
     PythonShell.run('main.py', options, function(err, results) {
         if(err) throw err;
-        var data = JSON.parse(results);
-        var element = document.getElementById("answer");
-        var array = '';
+        // var data = JSON.parse(results);
+        // var element = document.getElementById("answer");
+        // var array = '';
 
-        for (let i = 0; i < data['tree'].length; i++) {
-            array = array + i + '-->' + JSON.stringify(data['tree'][i]) + '<br />';
-        }
+        // for (let i = 0; i < data['tree'].length; i++) {
+        //     array = array + i + '-->' + JSON.stringify(data['tree'][i]) + '<br />';
+        // }
 
-        element.innerHTML= array;
+        // element.innerHTML= array;
+
+        console.log(results)
 
     });
 
 }
+
+python()
