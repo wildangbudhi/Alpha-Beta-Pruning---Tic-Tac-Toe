e24cdbaea6d3211248ecd09372718ea4cd3fec3d
Wildan Ghiffarie Budhi <wildangb2@gmail.com>
2019-03-28 20:50:27 +0700

    Bismillah

--- Js/renderer.js
+++ Js/renderer.js
@@ -3,22 +3,12 @@ const {PythonShell} = require('python-shell')
 function python() {
     // contoh data
     var data = [[1, 2, 3], [4, 6, 6], [7, 8, 9]];
-    var data2 = '[';
-    for (let i = 0; i < data.length; i++) {
-        data2 = data2 + '[';
-        for(let j = 0; j < data[i].length; j++){
-            data2 = data2 + data[i][j].toString();
-            if(j < data[i].length-1) data2 = data2 + ', ';
-        }
-        data2 = data2 + ']';
-        if(i < data.length-1) data2 = data2 + ', ';
-    }
-    data2 = data2 + ']';
+    data = JSON.stringify(data);
     
     let options = {
         pythonPath: 'C:/ProgramData/Anaconda3/python.exe',
         scriptPath: 'C:/Users/comp/Documents/Python/KB/Alpha-Beta-Pruning---Tic-Tac-Toe/Python/',
-        args : [data2]
+        args : [data]
     };
     
     PythonShell.run('coba.py', options, function(err, results) {
